737e6b021d063d96bbfe0c6926a1658adcc560da
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2017-12-14 17:55:40 -0300

    Added exponential distribution

---
+++ simulator/models/distributions/exponential.py
@@ -0,0 +1,15 @@
+import numpy
+
+class Exponential(Distribution):
+    
+    def __init__(self, avg, size = 10):
+        self.avg = avg
+        
+        self.size = size
+        self.generated_list = list()
+    
+    def get_next_value(self):
+        if(len(self.generated_list) == 0):
+            self.generated_list = numpy.random.exponential(self.avg, self.size)
+        
+        return self.generated_list.pop(0)
