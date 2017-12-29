1b1f94f33518e182c30879ce5502869ee2d0b11a
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2017-12-29 01:13:51 -0300

    Update config files and context

--- tests/context.py
+++ tests/context.py
@@ -2,5 +2,5 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-from simulator.models import Clients, GC, GCI, Request, LoadBalancer, Server, ServerWithGCI
+from models import Request, ClientLB, GCC, GCSTW, GCI, ServerBaseline, ServerControl
 from config import get_config
