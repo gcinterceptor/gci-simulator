0cce40a90842761b234d013b4db9b90eb76ace2e
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-01-23 08:35:47 -0300

    Update parameters

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 from models import ClientLB, ServerControl, ServerBaseline
 from config import get_config
-from log import log_request
+from log import log_request, log_gc
 import simpy, os, sys, time
+
 
 def create_directory(path):
     if not os.path.isdir(path):
@@ -32,9 +33,11 @@ def main():
     requests_conf = get_config('config/request.ini', 'request service_time-0.006 memory-0.001606664')
     server_conf = get_config('config/server.ini', 'server sleep_time-0.00001')
     if load == 'high':
+        delay = ' delay-0.102'
         loadbalancer_conf = get_config('config/clientlb.ini', 'clientlb sleep_time-0.00001 create_request_rate-150 max_requests-inf')
 
     elif load == 'low':
+        delay = ' delay-0.038'
         loadbalancer_conf = get_config('config/clientlb.ini', 'clientlb sleep_time-0.00001 create_request_rate-35 max_requests-inf')
 
     else:
@@ -46,12 +49,22 @@ def main():
     load_balancer = ClientLB(env, loadbalancer_conf, requests_conf, log_path)
     for i in range(NUMBER_OF_SERVERS):
         if scenario == 'control':
-            gc_conf = get_config('config/gcc.ini', 'gcc sleep_time-0.00001 threshold-0.9 collect_duration-0.0019 delay-1')
+            if load == 'high':
+                collect_duration = ' collect_duration-0.0035'
+            else:
+                collect_duration = ' collect_duration-0.004'
+
+            gc_conf = get_config('config/gcc.ini', 'gcc sleep_time-0.00001 threshold-0.9' + collect_duration + delay)
             gci_conf = get_config('config/gci.ini', 'gci sleep_time-0.00001 threshold-0.7 check_heap-10 initial_eget-0.9')
             server = ServerControl(env, i, server_conf, gc_conf, gci_conf, log_path)
 
         elif scenario == 'baseline':
-            gc_conf = get_config('config/gcc.ini', 'gcc sleep_time-0.00001 threshold-0.75 collect_duration-0.0019 delay-1')
+            if load == 'high':
+                collect_duration = ' collect_duration-0.019985'
+            else:
+                collect_duration = ' collect_duration-0.020383'
+
+            gc_conf = get_config('config/gcc.ini', 'gcc sleep_time-0.00001 threshold-0.75' + collect_duration + delay)
             server = ServerBaseline(env, i, server_conf, gc_conf, log_path)
 
         else:
@@ -60,11 +73,26 @@ def main():
         load_balancer.add_server(server)
         servers.append(server)
 
-    env.run(until=SIM_DURATION_SECONDS)
+    before_count = 0
+    before_time = 0
+    gc_count = list()  # how much gc executions was runned.
+    gc_time = list()  # how much time was used gcing.
+    for until in range(1, int(SIM_DURATION_SECONDS) + 1):
+        env.run(until=until)
+        gc_count.append(server.gc.collects_performed - before_count)
+        gc_time.append(server.gc.gc_exec_time_sum - before_time)
+        before_count = server.gc.collects_performed
+        before_time = server.gc.gc_exec_time_sum
+
+    if SIM_DURATION_SECONDS > int(SIM_DURATION_SECONDS): # it means that it is a float indeed.
+        env.run(until=SIM_DURATION_SECONDS) # We must run all duration
+
     log_request(load_balancer.requests, results_path, scenario, load)
+    log_gc(gc_count, gc_time, results_path, scenario, load)
 
     after = time.time()
     print("Time of simulation execution in seconds: %.4f" % (after - before))
+
 
 if __name__ == '__main__':
     main()
