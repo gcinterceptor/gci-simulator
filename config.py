f8115c193c76fa111c6d93c3960634c59c63d547
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-02-20 11:13:40 -0300

    Remove config files not needed for current design

--- config.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-def get_config(path_file, conf_section):
-    config_parser = configparser.ConfigParser()
-    config_parser.read(path_file)
-    required_conf = config_parser[conf_section]
-
-    return required_conf
