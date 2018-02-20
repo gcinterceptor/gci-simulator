c637ff304b3da0956ab3101aa02d3e7a7ecfcfb5
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-02-20 11:21:45 -0300

    Update main and bash script to execute simulations. It also update log module.

--- __main__.py
+++ __main__.py
@@ -1,12 +1,25 @@
 from models import LoadBalancer, ServerControl, ServerBaseline
-from config import get_config
-from log import log_request, log_gc
+from log import log_request, txt_writer
 import simpy, os, sys, time
 
 
 def create_directory(path):
     if not os.path.isdir(path):
         os.mkdir(path)
+
+
+def build_data(file_name, column):
+    data = list()
+    file_obj = open(file_name + ".csv", 'r')
+    for linha in file_obj:
+        try:
+            result = list(map(float, linha.split(",")))
+            data.append(result[column])
+        except:
+            # avoid invalid values.
+            pass
+
+    return data
 
 
 def main():
@@ -16,38 +29,40 @@ def main():
 
     NUMBER_OF_SERVERS = int(args[1])
     SIM_DURATION_SECONDS = float(args[2])
-    scenario = args[3]
-    load = args[4]
+    SCENARIO = args[3]
+    LOAD = int(args[4])
+    RESULTS_PATH = args[5]
+    create_directory(RESULTS_PATH)
 
-    if len(args) >= 6:
-        results_path = args[5]
-    else:
-        results_path = "results"
-    create_directory(results_path)
+    DATA_PATH = args[6]
+    SERVICE_TIME_FILE_NAME = args[7]
+    SERVICE_TIME_COLUMN = int(args[8])
+    service_time_data = build_data(DATA_PATH + SERVICE_TIME_FILE_NAME, SERVICE_TIME_COLUMN)
 
     env = simpy.Environment()
 
-    requests_conf = get_config('config/request.ini', 'request memory-0.001606664')
-    if load == 'high':
-        server_conf = get_config('config/server.ini', "server high")
-        loadbalancer_conf = get_config('config/loadbalancer.ini', 'clientlb sleep_time-0.00001 create_request_rate-150 max_requests-inf')
-
-    elif load == 'low':
-        server_conf = get_config('config/server.ini', "server low")
-        loadbalancer_conf = get_config('config/loadbalancer.ini', 'clientlb sleep_time-0.00001 create_request_rate-35 max_requests-inf')
+    if LOAD > 0:
+        loadbalancer_load = LOAD
 
     else:
         raise Exception("INVALID LOAD")
 
     servers = list()
-    load_balancer = LoadBalancer(loadbalancer_conf, requests_conf, env)
+    load_balancer = LoadBalancer(loadbalancer_load, env)
     for i in range(NUMBER_OF_SERVERS):
-        if scenario == 'control':
-            gci_conf = get_config('config/gci.ini', 'gci sleep_time-0.00001 threshold-0.7 check_heap-10 initial_get-0.9')
-            server = ServerControl(server_conf, gci_conf, env, i)
+        if SCENARIO == 'control':
+            PROCESSED_REQUESTS_FILE_NAME = args[10]
+            PROCESSED_REQUESTS_COLUMN = 0
+            processed_requests_data = build_data(DATA_PATH + PROCESSED_REQUESTS_FILE_NAME + str((i % 4) + 1), PROCESSED_REQUESTS_COLUMN)
 
-        elif scenario == 'baseline':
-            server = ServerBaseline(server_conf, env, i)
+            SHEDDED_REQUESTS_FILE_NAME = args[10]
+            SHEDDED_REQUESTS_COLUMN = 1
+            shedded_requests_data = build_data(DATA_PATH + SHEDDED_REQUESTS_FILE_NAME + str((i % 4) + 1), SHEDDED_REQUESTS_COLUMN)
+
+            server = ServerControl(env, i, service_time_data, processed_requests_data, shedded_requests_data)
+
+        elif SCENARIO == 'baseline':
+            server = ServerBaseline(env, i, service_time_data)
 
         else:
             raise Exception("INVALID SCENARIO")
@@ -55,28 +70,35 @@ def main():
         load_balancer.add_server(server)
         servers.append(server)
 
-    before_count = NUMBER_OF_SERVERS * [0]
-    before_time = NUMBER_OF_SERVERS * [0]
-    gc_count = list()  # Number of GC executions per second.
-    gc_time = list()  # Amount of time collecting garbage per second.
     for until in range(1, int(SIM_DURATION_SECONDS) + 1):
         env.run(until=until)
-
-        index = 0
-        for server in servers:
-            gc_count.append(server.collects_performed - before_count[index])
-            gc_time.append(server.gc_exec_time_sum - before_time[index])
-            before_count[index] = server.collects_performed
-            before_time[index] = server.gc_exec_time_sum
-            index += 1
 
     if SIM_DURATION_SECONDS > int(SIM_DURATION_SECONDS): # it means that it is a float indeed.
         env.run(until=SIM_DURATION_SECONDS) # We must run all duration
 
-    log_request(load_balancer.requests, results_path, scenario, load)
-    log_gc(gc_count, gc_time, results_path, scenario, load)
+    for request in load_balancer.requests:
+        request._latency = request._latency
+
+    EXPERIMENT_NUMBER = args[9]
+    log_request(load_balancer.requests, RESULTS_PATH, str(NUMBER_OF_SERVERS) + "instances_loadbalancer_request_status", SCENARIO, str(LOAD) + "_" + EXPERIMENT_NUMBER)
 
     after = time.time()
+
+    text = "number of service instances: " + str(NUMBER_OF_SERVERS) \
+           + "\nsimulation time duration: " + str(SIM_DURATION_SECONDS) \
+           + "\nconfiguration scenario: " + SCENARIO \
+           + "\nworkload per instance: " + str(LOAD) + "req/sec" \
+           + "\ngeneral workload: " + str(LOAD * NUMBER_OF_SERVERS) + "req/sec" \
+           + "\ncreated requests: " + str(load_balancer.created_requests) \
+           + "\nshedded requests: " + str(load_balancer.shedded_requests) \
+           + "\nlost requests: " + str(load_balancer.lost_requests) \
+           + "\nsucceeded requests: " + str(load_balancer.succeeded_requests)
+    txt_writer(RESULTS_PATH + "/simulation_info_" + SCENARIO + "_load_" + str(LOAD) + ".txt", text)
+
+    print("created requests: %f" % load_balancer.created_requests)
+    print("shedded requests: %f" % load_balancer.shedded_requests)
+    print("lost requests: %f" % load_balancer.lost_requests)
+    print("succeeded requests: %f" % load_balancer.succeeded_requests)
     print("Time of simulation execution in seconds: %.4f" % (after - before))
 
 
