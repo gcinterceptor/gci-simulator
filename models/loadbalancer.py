5033944f9e10c63f2c7d46cbd82a73ec40d20ac6
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-02-20 11:19:09 -0300

    Update models to new design. There is no GC or GCI models.

--- models/loadbalancer.py
+++ models/loadbalancer.py
@@ -3,13 +3,15 @@ from .request import Request
 
 class LoadBalancer(object):
 
-    def __init__(self, conf, requests_conf, env, server=None):
-        self.sleep = float(conf['sleep_time'])
+    def __init__(self, create_request_rate, env, server=None):
+        self.create_request_rate = create_request_rate
         self.env = env
+
+        self.comunication_delay = 0.001 # 1ms
+        self.sleep = 0.00001 #  0.01ms
 
         self.requests = list()
         self.servers = []
-        self.server_availability = {}
         self.add_server(server)
 
         self.created_requests = 0
@@ -19,35 +21,31 @@ class LoadBalancer(object):
 
         self.server_index = 0
         self.is_available = True
-        self.create_request = env.process(self.create_and_forward_requests(float(conf['max_requests']), int(conf['create_request_rate']), requests_conf))
+        self.create_request = env.process(self.create_and_forward_requests())
 
-    def create_and_forward_requests(self, max_requests, create_request_rate, requests_conf):
-        time_between_each_sending = 1 / create_request_rate
-        while self.created_requests < max_requests:
-            request = Request(requests_conf, self.env, self.created_requests, self.env.now, self)
+    def create_and_forward_requests(self):
+        time_between_each_sending = 1 / (self.create_request_rate * len(self.servers))
+        while True:
+            request = Request(self.env, self.created_requests, self.env.now, self)
             self.created_requests += 1
-            self.env.process(self.forward(self.server_index, request))
+            self.env.process(self.forward(request))
             yield self.env.timeout(time_between_each_sending)
 
-    def forward(self, server_index, request):
-        yield self.env.process(self.servers[server_index].request_arrived(request))
+    def forward(self, request):
+        yield self.env.timeout(self.comunication_delay)
+        request.times_forwarded += 1
+        self.env.process(self.servers[self.server_index].request_arrived(request))
         self.server_index = (self.server_index + 1) % len(self.servers)
 
-    def shed_request(self, request, server, unavailable_until):
+    def shed_request(self, request):
         self.shedded_requests += 1
-        self.server_availability[server.id] = self.env.now + unavailable_until
-
-        server_index, count, forward = self.server_index, 0, False
-        while count < len(self.servers) and not forward:
-            if self.env.now >= self.server_availability[self.servers[server_index].id]:
-                self.env.process(self.forward(server_index, request))
-                forward = True
-            else:
-                server_index = (server_index + 1) % len(self.servers)
-            count += 1
-
-        if not forward:
+        if request.times_forwarded == len(self.servers):
             self.lost_requests += 1
+            request.finished_at(self.env.now)
+            self.requests.append(request)
+
+        else:
+            self.env.process(self.forward(request))
 
         yield self.env.timeout(self.sleep)
 
@@ -60,4 +58,3 @@ class LoadBalancer(object):
     def add_server(self, server):
         if server:
             self.servers.append(server)
-            self.server_availability[server.id] = 0
