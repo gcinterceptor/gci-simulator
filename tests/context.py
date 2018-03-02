a17999ec01bd640fae0809281f7e9ec32dd912d2
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-03-02 11:14:00 -0300

    Update module of imports

--- tests/context.py
+++ tests/context.py
@@ -2,5 +2,4 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-from models import Request, ClientLB, GCC, GCSTW, GCI, ServerBaseline, ServerControl
-from config import get_config
+from models import LoadBalancer, Request, ServerBaseline, ServerControl, Distribution, Reproduction
