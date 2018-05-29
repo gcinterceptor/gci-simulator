3b16342621098d37ddd21653fdd61402ca3b6b64
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2018-05-29 13:44:22 -0300

    Update bash script.

--- run_simulator.sh
+++ run_simulator.sh
@@ -6,36 +6,22 @@ set -x
 echo "ROUND_START: ${ROUND_START:=1}"
 echo "ROUND_END: ${ROUND_END:=1}"
 echo "INSTANCES: ${ISNTANCES:=1 2 4}"
-echo "DURATION: ${DURATION:=120}"
-echo "LOAD_PER_INSTANCE: ${LOAD_PER_INSTANCE:=80}"
-echo "SCENARIO: ${SCENARIO:=control}"
-echo "RESULTS_PATH: ${RESULTS_PATH:=/tmp/simulation/${NUMBER_OF_SERVERS}i/${SCENARIO}/}"
+echo "DURATION: ${DURATION:=300}"
+echo "LOAD_PER_INSTANCE: ${LOAD_PER_INSTANCE:=40}"
+echo "RESULTS_PATH: ${RESULTS_PATH:=/tmp/simulation/}"
 mkdir -p $RESULTS_PATH
 
-echo "RESULTS_NAME: ${RESULTS_NAME}"
+echo "GENERAL_RESULTS_NAME: ${GENERAL_RESULTS_NAME}"
 echo "DATA_PATH: ${DATA_PATH}"
 echo "SERVICE_TIME_FILE_NAME: ${SERVICE_TIME_FILE_NAME}"
-echo "SERVICE_TIME_DATA_COLUMN: ${SERVICE_TIME_DATA_COLUMN}"
-echo "SHEDDING_FILE_NAME: ${SHEDDING_FILE_NAME}"
-echo "SHEDDING_NUMBER_OF_FILES: ${SHEDDING_NUMBER_OF_FILES}"
-echo "SETUP_INFO: ${SETUP_INFO:=setup_info.txt}"
-
 
 for instance in ${INSTANCES};
 do
 	for round in `seq ${ROUND_START} ${ROUND_END}`
 	do
-		echo ""
-		echo "round ${round}: Simulating ${instance} instance(s)..."
-		LOAD=$(($LOAD_PER_INSTANCE * $instance)) NUMBER_OF_SERVERS=$instance python3 __main__.py
-		mkdir -p $RESULTS_PATH${instance}i/
-		mv "${RESULTS_PATH}${RESULTS_NAME}.csv" "${RESULTS_PATH}${instance}i/${RESULTS_NAME}_${round}.csv"
-		echo "round ${round}: Finished."
-		echo ""
-	done 
-	
-	INFO="number of service instances: ${instance}\nsimulation time duration: ${DURATION}\nconfiguration scenario: ${SCENARIO}\nworkload per instance: ${LOAD_PER_INSTANCE}req/sec\ngeneral workload: $(($LOAD_PER_INSTANCE * ${instance}))req/sec"
-	echo -e $INFO > $RESULTS_PATH${instance}i/$SETUP_INFO
-	
+		echo "\nround ${round}: Simulating ${instance} instance(s)..."
+		NUMBER_OF_SERVERS=$instance LOAD=$(($LOAD_PER_INSTANCE * $instance)) RESULTS_NAME="${GENERAL_RESULTS_NAME}_${DURATION}_${LOAD}_${instance}_${round}" python3 __main__.py
+		echo "round ${round}: Finished.\n"
+	done
 done
 
