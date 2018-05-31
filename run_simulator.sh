f1f79ba7208e8e73f39404692147891852bbaa35
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-05-31 09:46:46 -0300

    Rename GENERAL_RESULTS_NAME to PREFIX_RESULTS_NAME in run_simulator.

--- run_simulator.sh
+++ run_simulator.sh
@@ -4,14 +4,14 @@ date
 set -x
 
 echo "ROUND_START: ${ROUND_START:=1}"
-echo "ROUND_END: ${ROUND_END:=1}"
+echo "ROUND_END: ${ROUND_END:=4}"
 echo "INSTANCES: ${INSTANCES:=1 2 4}"
 echo "DURATION: ${DURATION:=300}"
 echo "LOAD_PER_INSTANCE: ${LOAD_PER_INSTANCE:=40}"
 echo "RESULTS_PATH: ${RESULTS_PATH:=/tmp/simulation/}"
 mkdir -p $RESULTS_PATH
 
-echo "GENERAL_RESULTS_NAME: ${GENERAL_RESULTS_NAME}"
+echo "PREFIX_RESULTS_NAME: ${PREFIX_RESULTS_NAME}"
 echo "DATA_PATH: ${DATA_PATH}"
 echo "INPUT_FILE_NAME: ${INPUT_FILE_NAME}"
 echo "DEBBUG_LOG: ${DEBBUG_LOG:='false'}"
@@ -22,7 +22,7 @@ do
 	do
     	echo ""
 		echo "round ${round}: Simulating ${instance} instance(s)..."
-	    NUMBER_OF_SERVERS=$instance DURATION=$DURATION LOAD=$(($LOAD_PER_INSTANCE * $instance)) RESULTS_PATH=$RESULTS_PATH DATA_PATH=$DATA_PATH INPUT_FILE_NAME=$INPUT_FILE_NAME RESULTS_NAME="${GENERAL_RESULTS_NAME}_${DURATION}_${LOAD}_${instance}_${round}" DEBBUG_LOG=$DEBBUG_LOG python3 __main__.py
+	    NUMBER_OF_SERVERS=$instance DURATION=$DURATION LOAD=$(($LOAD_PER_INSTANCE * $instance)) RESULTS_PATH=$RESULTS_PATH DATA_PATH=$DATA_PATH INPUT_FILE_NAME=$INPUT_FILE_NAME RESULTS_NAME="${PREFIX_RESULTS_NAME}_${DURATION}_${LOAD}_${instance}_${round}" DEBBUG_LOG=$DEBBUG_LOG python3 __main__.py
 		echo "round ${round}: Finished."
 		echo ""
 	done
