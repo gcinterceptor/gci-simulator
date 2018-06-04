5829d68815d7d66a797841cd420a84cac0f6cfee
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-06-04 11:04:38 -0300

    Remove unnecessary column.

--- log.py
+++ log.py
@@ -24,11 +24,11 @@ def log_debbug(requests, debbug_path, debbug_name, mode):
     data = []
     if mode == "w":
         data.append(["id", "timestamp", "created_time", "latency",
-                     "service_time", "status", "done", "times_forwarded", "hops"])
+                     "service_time", "status", "times_forwarded", "hops"])
 
     for request in requests:
         data.append([request.id, request.finished_time * 1000, request.created_time, request._latency,
-                     request.service_time, request.status, request.done, request.times_forwarded, " ".join(request.hops)])
+                     request.service_time, request.status, request.times_forwarded, " ".join(request.hops)])
 
     file_path = debbug_path + "/" + debbug_name + "_debbug.csv"
     csv_writer(data, file_path, mode)
