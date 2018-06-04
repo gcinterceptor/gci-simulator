e4ad602f56fc5298eaf0c3019f2d3e92497ea978
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-06-04 17:11:25 -0300

    Update to receive list of file input names and add echo with colors.

--- run_simulator.sh
+++ run_simulator.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 date
 set -x
+
+echo -e "${GREEN}STARTING SIMULATION${NC}"
 
 echo "ROUND_START: ${ROUND_START:=1}"
 echo "ROUND_END: ${ROUND_END:=4}"
@@ -13,19 +21,20 @@ mkdir -p $RESULTS_PATH
 
 echo "PREFIX_RESULTS_NAME: ${PREFIX_RESULTS_NAME}"
 echo "DATA_PATH: ${DATA_PATH}"
-echo "INPUT_FILE_NAME: ${INPUT_FILE_NAME}"
-echo "NUMBER_OF_INPUT_FILES: ${NUMBER_OF_INPUT_FILES}"
+echo -e "${YELLOW}INPUT_FILE_NAMES: ${INPUT_FILE_NAMES}${NC}"
 echo "DEBBUG_LOG: ${DEBBUG_LOG:='false'}"
 
 for instance in ${INSTANCES};
 do
+	echo -e "${GREEN}SIMULATING ${instance} INSTANCE(S)...${NC}"
 	for round in `seq ${ROUND_START} ${ROUND_END}`
 	do
-    	echo ""
-		echo "round ${round}: Simulating ${instance} instance(s)..."
-	    NUMBER_OF_SERVERS=$instance DURATION=$DURATION LOAD=$(($LOAD_PER_INSTANCE * $instance)) RESULTS_PATH=$RESULTS_PATH DATA_PATH=$DATA_PATH INPUT_FILE_NAME=$INPUT_FILE_NAME NUMBER_OF_INPUT_FILES=$NUMBER_OF_INPUT_FILES RESULTS_NAME="${PREFIX_RESULTS_NAME}_${DURATION}_${LOAD}_${instance}_${round}" DEBBUG_LOG=$DEBBUG_LOG python3 __main__.py
+		echo ""
+		echo -e "${BLUE}ROUND ${round}!${NC}"
+		NUMBER_OF_SERVERS=$instance DURATION=$DURATION LOAD=$(($LOAD_PER_INSTANCE * $instance)) DATA_PATH=$DATA_PATH INPUT_FILE_NAMES=$INPUT_FILE_NAMES RESULTS_PATH=$RESULTS_PATH RESULTS_NAME="${PREFIX_RESULTS_NAME}_${DURATION}_${LOAD}_${instance}_${round}" DEBBUG_LOG=$DEBBUG_LOG python3 __main__.py
 		echo "round ${round}: Finished."
 		echo ""
 	done
 done
 
+echo -e "${RED}SIMULATION FINISHED${NC}"
