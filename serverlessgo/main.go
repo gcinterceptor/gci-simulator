0c9ed0d06aad7affdc85650e518ac32dd5ee877b
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-08-01 17:49:07 -0300

    Add first load balancer implementation.

--- serverlessgo/main.go
+++ serverlessgo/main.go
@@ -15,25 +15,65 @@ var (
 	lambda   = flag.Float64("lambda", 140.0, "The lambda of the Poisson distribution used on workload.")
 )
 
+var arrivalQueue = godes.NewFIFOQueue("arrival")
+var arrivalCond = godes.NewBooleanControl()
+
 func main() {
 	fmt.Println("Simulation Started")
 	flag.Parse()
 	
-	arrivalQueue := godes.NewFIFOQueue("arrival")
+	lb := newLoadBalancer()
+	godes.AddRunner(lb)
+
 	poissonDist := &distuv.Poisson{*lambda, rand.NewSource(uint64(time.Now().Nanosecond()))}
 	reqID := int64(0)
 	godes.Run()
 	for godes.GetSystemTime() < duration.Seconds() {
 		arrivalQueue.Place(&request{id: reqID})
+		arrivalCond.Set(true)
 		interArrivalTime := poissonDist.Rand()
 		godes.Advance(interArrivalTime)
-		r := arrivalQueue.Get().(*request)
-		r.responseTime = interArrivalTime 
 		reqID++
 	}
 
+	lb.terminate()
 	godes.WaitUntilDone()
 	fmt.Println("Simulation Ended")
+}
+
+type loadBalancer struct {
+	*godes.Runner
+	finishedRequests *FIFOQueue
+	isTerminated bool
+}
+
+func newLoadBalancer() *loadBalancer {
+	return &loadBalancer{&godes.Runner{}, false}
+}
+
+func (lb *loadBalancer) Run() {
+	fmt.Println("id,status,latency")
+	for {
+		arrivalCond.Wait(true)
+		if arrivalQueue.Len() > 0 {
+			r := arrivalQueue.Get().(*request)
+			if r.status != 200 {
+				r.status = 200
+				r.responseTime = *lambda // temporary value
+				arrivalQueue.Place(r)
+			} else {
+				fmt.Printf("%d,%d,%.1f\n", r.id, r.status, r.responseTime*1000)
+				lb.finishedRequests.Place(r)
+			}
+		}
+
+		if arrivalQueue.Len() == 0 {
+			if lb.isTerminated {
+				break
+			}
+			arrivalCond.Set(false)
+		}
+	}
 }
 
 type request struct {
