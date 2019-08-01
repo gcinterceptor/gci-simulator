2bb96941e53d74caf83e400ebe23026ad1683c1a
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-08-01 18:38:23 -0300

    Apply requested change

--- serverlessgo/main.go
+++ serverlessgo/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/agoussia/godes"
+
 	"golang.org/x/exp/rand"
 	"gonum.org/v1/gonum/stat/distuv"
 )
@@ -14,9 +15,6 @@ var (
 	duration = flag.Duration("d", 300*time.Second, "Duration of the simulation.")
 	lambda   = flag.Float64("lambda", 140.0, "The lambda of the Poisson distribution used on workload.")
 )
-
-var arrivalQueue = godes.NewFIFOQueue("arrival")
-var arrivalCond = godes.NewBooleanControl()
 
 func main() {
 	flag.Parse()
@@ -28,8 +26,7 @@ func main() {
 	reqID := int64(0)
 	godes.Run()
 	for godes.GetSystemTime() < duration.Seconds() {
-		arrivalQueue.Place(&request{id: reqID, creatingTimestamp: godes.GetSystemTime()})
-		arrivalCond.Set(true)
+		sendRequestToLoadBalancer(lb, &request{id: reqID, creatingTimestamp: godes.GetSystemTime()})
 		interArrivalTime := poissonDist.Rand()
 		godes.Advance(interArrivalTime)
 		reqID++
@@ -42,39 +39,46 @@ func main() {
 type loadBalancer struct {
 	*godes.Runner
 	isTerminated bool
+	arrivalQueue *godes.FIFOQueue
+	arrivalCond *godes.BooleanControl
 }
 
 func newLoadBalancer() *loadBalancer {
-	return &loadBalancer{&godes.Runner{}, false}
+	return &loadBalancer{&godes.Runner{}, false, godes.NewFIFOQueue("arrival"), godes.NewBooleanControl()}
+}
+
+func sendRequestToLoadBalancer(lb *loadBalancer, r *request) {
+	lb.receiveRequest(r)
+}
+
+func (lb *loadBalancer) receiveRequest(r *request) {
+	lb.arrivalQueue.Place(r)
+	lb.arrivalCond.Set(true)
 }
 
 func (lb *loadBalancer) terminate() {
-	arrivalCond.Set(true)
+	lb.arrivalCond.Set(true)
 	lb.isTerminated = true
 }
 
 func (lb *loadBalancer) Run() {
 	fmt.Println("creatingTimestamp,finishingTimestamp,id,status,latency")
 	for {
-		arrivalCond.Wait(true)
-		if arrivalQueue.Len() > 0 {
-			r := arrivalQueue.Get().(*request)
-			if r.status != 200 {
-				r.status = 200
-				r.responseTime = *lambda // temporary value
-				godes.Advance(*lambda)
-				r.finishingTimestamp = godes.GetSystemTime()
-				arrivalQueue.Place(r)
-			} else {
-				fmt.Printf("%.1f,%.1f,%d,%d,%.1f\n", r.creatingTimestamp, r.finishingTimestamp, r.id, r.status, r.responseTime*1000)
-			}
+		lb.arrivalCond.Wait(true)
+		if lb.arrivalQueue.Len() > 0 {
+			r := lb.arrivalQueue.Get().(*request)
+			r.status = 200
+			r.responseTime = *lambda // temporary value
+			godes.Advance(*lambda)
+			r.finishingTimestamp = godes.GetSystemTime()
+			fmt.Printf("%.1f,%.1f,%d,%d,%.1f\n", r.creatingTimestamp, r.finishingTimestamp, r.id, r.status, r.responseTime*1000)
 		}
 
-		if arrivalQueue.Len() == 0 {
+		if lb.arrivalQueue.Len() == 0 {
 			if lb.isTerminated {
 				break
 			}
-			arrivalCond.Set(false)
+			lb.arrivalCond.Set(false)
 		}
 	}
 }
