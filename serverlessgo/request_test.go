0af927926a6b6419ca528768629dfb7ed5f682b4
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-08-12 14:56:16 -0300

    Fix request test

--- serverlessgo/request_test.go
+++ serverlessgo/request_test.go
@@ -5,16 +5,16 @@ import (
 	"testing"
 )
 
-func testHasBeenProcessed(t *testing.T) {
+func TestHasBeenProcessed(t *testing.T) {
 	var testData = []struct {
 		desc      string
 		request   *request
 		instances []int
 		want      []bool
 	}{
-		{"EmptyHop", &request{}, []int{1}, []bool{true}},
+		{"EmptyHop", &request{}, []int{1}, []bool{false}},
 		{"OneHopTrue", &request{hops: []int{1}}, []int{1}, []bool{true}},
-		{"OneHopFalse", &request{hops: []int{1}}, []int{1}, []bool{true}},
+		{"OneHopFalse", &request{hops: []int{1}}, []int{2}, []bool{false}},
 		{"ManyHopsTrue", &request{hops: []int{1, 2, 3}}, []int{1, 2, 3}, []bool{true, true, true}},
 		{"ManyHopsFalse", &request{hops: []int{1, 2, 3}}, []int{5, 6, 7}, []bool{false, false, false}},
 		{"ManyHopsMixed", &request{hops: []int{1, 2}}, []int{1, 3, 2, 4}, []bool{true, false, true, false}},
