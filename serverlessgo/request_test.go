a3ead6fa25f2be7e6097376ec0cd4d58fcb9e316
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-08-14 18:03:39 -0300

    Update request tests

--- serverlessgo/request_test.go
+++ serverlessgo/request_test.go
@@ -30,3 +30,69 @@ func TestHasBeenProcessed(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateHops(t *testing.T) {
+	type data struct {
+		desc    string
+		request *request
+		value   int
+		want    []int
+	}
+	var updateHop = []data{
+		{"UpdateEmptyHop", &request{}, 1, []int{1}},
+		{"UpdateNotEmptyHop", &request{hops: []int{0, 5}}, 2, []int{0, 5, 2}},
+	}
+	for _, d := range updateHop {
+		t.Run(d.desc, func(t *testing.T) {
+			d.request.updateHops(d.value)
+			got := d.request.hops
+			if !reflect.DeepEqual(d.want, got) {
+				t.Fatalf("Want: %v, got: %v", d.want, got)
+			}
+		})
+	}
+}
+
+func TestUpdateStatus(t *testing.T) {
+	type data struct {
+		desc    string
+		request *request
+		value   int
+		want    int
+	}
+	var updateHop = []data{
+		{"DefaultStatus", &request{}, 503, 503},
+		{"NotDefaultStatus", &request{status: 503}, 200, 200},
+	}
+	for _, d := range updateHop {
+		t.Run(d.desc, func(t *testing.T) {
+			d.request.updateStatus(d.value)
+			got := d.request.status
+			if !reflect.DeepEqual(d.want, got) {
+				t.Fatalf("Want: %v, got: %v", d.want, got)
+			}
+		})
+	}
+}
+
+func TestUpdateResponseTime(t *testing.T) {
+	type data struct {
+		desc    string
+		request *request
+		value   float64
+		want    float64
+	}
+	var updateHop = []data{
+		{"DefaultResponse", &request{}, 0.5, 0.5},
+		{"NotDefaultResponse", &request{responseTime: 1.5}, 0.8, 2.3},
+	}
+	for _, d := range updateHop {
+		t.Run(d.desc, func(t *testing.T) {
+			d.request.updateResponseTime(d.value)
+			got := d.request.responseTime
+			if !reflect.DeepEqual(d.want, got) {
+				t.Fatalf("Want: %v, got: %v", d.want, got)
+			}
+		})
+	}
+}
