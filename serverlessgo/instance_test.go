efc38eb83ad93f7126c459c18ab5abc0fb95b516
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-09-11 17:02:51 -0300

    Update run instance test

--- serverlessgo/instance_test.go
+++ serverlessgo/instance_test.go
@@ -82,59 +82,28 @@ func TestScaleDown(t *testing.T) {
 	}
 }
 
-type TestLoadBalancer struct{ reqsResponsed int }
+type TestLoadBalancer struct{ req *Request }
 
-func (lb *TestLoadBalancer) response(r *Request) { lb.reqsResponsed++ }
+func (lb *TestLoadBalancer) response(r *Request) { lb.req = r }
 func TestInstanceRun(t *testing.T) {
-	type Want struct {
-		count int
-		reqs  []*Request
+	instance := &Instance{
+		Runner:  &godes.Runner{},
+		cond:    godes.NewBooleanControl(),
+		reproducer: newInputReproducer([]inputEntry{{200, 0.8}, {200, 0.1}, {200, 0.2}}),
+		lb:      &TestLoadBalancer{},
 	}
-	type TestData struct {
-		desc     string
-		instance *Instance
-		advance  float64
-		reqs     []*Request
-		want     *Want
-	}
-	var testData = []TestData{
-		{"OneEntry", &Instance{
-			Runner:  &godes.Runner{},
-			cond:    godes.NewBooleanControl(),
-			entries: []inputEntry{{200, 0.3}},
-			lb:      &TestLoadBalancer{},
-		}, 0.5, []*Request{{id: 1}, {id: 2}, {id: 3}},
-			&Want{3, []*Request{
-				{1, 200, 0.3, []int{0}},
-				{2, 200, 0.3, []int{0}},
-				{3, 200, 0.3, []int{0}}}}},
-		{"ManyEntries", &Instance{
-			Runner:  &godes.Runner{},
-			cond:    godes.NewBooleanControl(),
-			entries: []inputEntry{{200, 0.8}, {200, 0.1}, {200, 0.2}, {200, 0.3}},
-			lb:      &TestLoadBalancer{},
-		}, 0.9, []*Request{{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7}, {id: 8}},
-			&Want{8, []*Request{
-				{1, 200, 0.8, []int{0}}, {2, 200, 0.1, []int{0}},
-				{3, 200, 0.2, []int{0}}, {4, 200, 0.3, []int{0}},
-				{5, 200, 0.1, []int{0}}, {6, 200, 0.2, []int{0}},
-				{7, 200, 0.3, []int{0}}, {8, 200, 0.1, []int{0}}}}},
-	}
-	for _, d := range testData {
-		t.Run(d.desc, func(t *testing.T) {
-			godes.Run()
-			godes.AddRunner(d.instance)
-			for _, r := range d.reqs {
-				godes.Advance(d.advance)
-				d.instance.receive(r)
-			}
-			godes.WaitUntilDone()
-			godes.Clear()
 
-			got := &Want{d.instance.lb.(*TestLoadBalancer).reqsResponsed, d.reqs}
-			if !reflect.DeepEqual(d.want, got) {
-				t.Fatalf("Want: %v, got: %v", d.want, got)
-			}
-		})
+	godes.Run()
+	godes.AddRunner(instance)
+	godes.Advance(0.8)
+	
+	req := &Request{id: 1}
+	instance.receive(req)
+	godes.WaitUntilDone()
+	
+	want := req.id
+	got := instance.lb.(*TestLoadBalancer).req.id
+	if want != got {
+		t.Fatalf("Want: %v, got: %v", want, got)
 	}
 }
