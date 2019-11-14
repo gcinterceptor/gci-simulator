4e9738bf131f104ba361a159ab1380c2e1246341
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-14 11:47:18 -0300

    Update output.go

--- serverlessgo/output.go
+++ serverlessgo/output.go
@@ -15,21 +15,21 @@ type outputWriter struct {
 func newOutputWriter(path, header string) (*outputWriter, error) {
 	f, err := os.Create(path)
 	if err != nil {
-		return nil, fmt.Errorf("Error trying to create the output file: %q", err)
+		return nil, fmt.Errorf("Error trying to create the reqs output file: %q", err)
 	}
 	_, err = f.WriteString(header)
 	if err != nil {
-		return nil, fmt.Errorf("Error trying to write the csv header: %q", err)
+		return nil, fmt.Errorf("Error trying to write the csv reqs header: %q", err)
 	}
 	return &outputWriter{f: f}, nil
 }
 
 func (o *outputWriter) RequestFinished(r *sim.Request) {
-	s := fmt.Sprintf("%d,%d,%.1f\n", r.ID, r.Status, r.ResponseTime*1000)
+	s := fmt.Sprintf("%d,%d,%f,%f,%v\n", r.ID, r.Status, r.CreatedTime, r.ResponseTime*1000, r.Hops)
 	_, err := o.f.WriteString(s)
 	if err != nil {
 		// Crash the simulation binary if we can not write output.
-		log.Fatalf("Error trying to write s (%s) in file (%v+): %q", s, o.f, err)
+		log.Fatalf("Error trying to write req s (%s) in file (%v+): %q", s, o.f, err)
 	}
 }
 
@@ -37,9 +37,53 @@ func (o *outputWriter) close() {
 	o.f.Close()
 }
 
-func printSimulationMetrics(throughput float64, totalCost, totalEfficiency float64, simulationTime int64) {
-	fmt.Printf("Throughput: %f\n", throughput)
-	fmt.Printf("Total cost of instances: %.5f\n", totalCost)
-	fmt.Printf("Total efficiency of instances: %.10f\n", totalEfficiency)
-	fmt.Printf("time running the simulation: %d seconds\n", simulationTime)
+func saveSimulationMetrics(path string, res sim.Results) error {
+	throughput := float64(res.RequestCount)/(*duration).Seconds()
+	totalCost := res.Cost
+	totalEfficiency := res.Efficiency
+	simulationTime := res.SimulationTime
+
+	s := fmt.Sprintf("Throughput: %f\n", throughput)
+	s += fmt.Sprintf("Total cost of instances: %.5f\n", totalCost)
+	s += fmt.Sprintf("Total efficiency of instances: %.10f\n", totalEfficiency)
+	s += fmt.Sprintf("time running the simulation: %d seconds\n", simulationTime)
+	
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("Error trying to create the output file: %q", err)
+	}
+	_, err = f.WriteString(s)
+	if err != nil {
+		return fmt.Errorf("Error trying to write the csv metrics: %q", err)
+	}
+	f.Close()
+
+	return nil	
+}
+
+func saveSimulationInstances(path string, instances []sim.IInstance) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("Error trying to create the output file: %q", err)
+	}
+
+	s := fmt.Sprintf("id,is_terminated,is_working,is_available,lastWorked,busyTime,up_time,idle_time,efficiency,created_time\n")
+	_, err = f.WriteString(s)
+	if err != nil {
+		return fmt.Errorf("Error trying to write the csv instances header: %q", err)
+	}
+	for _, i := range instances {
+		s = fmt.Sprintf(
+			"%d,%t,%t,%t,%f,%f,%f,%f,%f,%f\n",
+			i.GetId(), i.IsTerminated(), i.IsWorking(), i.IsAvailable(),
+			i.GetLastWorked(), i.GetBusyTime(), i.GetUpTime(),
+			i.GetIdleTime(), i.GetEfficiency(), i.GetCreatedTime(),
+		)
+		_, err = f.WriteString(s)
+		if err != nil {
+			return fmt.Errorf("Error trying to write the csv instances: %q", err)
+		}
+	}
+
+	return nil	
 }
