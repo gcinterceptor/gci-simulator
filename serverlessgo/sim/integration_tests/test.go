59105b1d79d9450cf5aad84d40b4fa21cdfdbd96
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-23 12:09:19 -0300

    Update integration tests

--- serverlessgo/sim/integration_tests/test.go
+++ serverlessgo/sim/integration_tests/test.go
@@ -26,23 +26,23 @@ func main() {
 		{{200, 0.011, "body", 0, 0.011}},
 		{{200, 0.005, "body", 0, 0.005}, {200, 0.005, "body", 0, 0.005}, {503, 0.0002, "body", 0, 0.0002}},
 	}
-	optimized := false
+	scheduler := 0
 	var simulatedRequests collectorListener
 	warmup := 0
-	res := sim.Run(duration, idlenessDeadline, sim.NewConstantInterArrival(0.01), input, &simulatedRequests, optimized, warmup)
+	res := sim.Run(duration, idlenessDeadline, sim.NewConstantInterArrival(0.01), input, &simulatedRequests, scheduler, warmup)
 
 	if len(res.Instances) != 5 {
 		log.Fatalf("number of instances - want:5 got:%+v", len(res.Instances))
 	}
 	expectedRequests := []sim.Request{
-		{ID: 0, Status: 200, CreatedTime: 0.00, ResponseTime: 0.015, Hops: []int{0}},     // response time from {200, 0.015} of instance 0
-		{ID: 1, Status: 200, CreatedTime: 0.01, ResponseTime: 0.011, Hops: []int{1}},     // response time from {200, 0.011} of instance 1
-		{ID: 2, Status: 200, CreatedTime: 0.02, ResponseTime: 0.008, Hops: []int{0}},     // response time from {200, 0.008} of instance 0
-		{ID: 3, Status: 200, CreatedTime: 0.03, ResponseTime: 0.0051, Hops: []int{0, 2}}, // response time from {503, 0.0001} of instance 0 plus {200, 0.005} of instance 2
-		{ID: 4, Status: 200, CreatedTime: 0.04, ResponseTime: 0.005, Hops: []int{2}},     // response time from {200, 0.005} of instance 2
-		{ID: 5, Status: 200, CreatedTime: 0.05, ResponseTime: 0.0152, Hops: []int{2, 3}}, // response time from {503, 0.0002} of instance 0 plus {200, 0.015} of instance 3
-		{ID: 6, Status: 200, CreatedTime: 0.06, ResponseTime: 0.011, Hops: []int{4}},     // response time from {200, 0.011} of instance 4
-		{ID: 7, Status: 200, CreatedTime: 0.07, ResponseTime: 0.008, Hops: []int{3}},     // response time from {200, 0.008} of instance 3
+		{ID: 0, Status: 200, CreatedTime: 0.00, ResponseTime: 0.015, Hops: []string{"i0-f0"}},        // response time from {200, 0.015} of instance 0
+		{ID: 1, Status: 200, CreatedTime: 0.01, ResponseTime: 0.011, Hops: []string{"i1-f1"}},        // response time from {200, 0.011} of instance 1
+		{ID: 2, Status: 200, CreatedTime: 0.02, ResponseTime: 0.008, Hops: []string{"i0-f0"}},        // response time from {200, 0.008} of instance 0
+		{ID: 3, Status: 200, CreatedTime: 0.03, ResponseTime: 0.0051, Hops: []string{"i0-f0", "i2-f2"}}, // response time from {503, 0.0001} of instance 0 plus {200, 0.005} of instance 2
+		{ID: 4, Status: 200, CreatedTime: 0.04, ResponseTime: 0.005, Hops: []string{"i2-f2"}},        // response time from {200, 0.005} of instance 2
+		{ID: 5, Status: 200, CreatedTime: 0.05, ResponseTime: 0.0152, Hops: []string{"i2-f2", "i3-f0"}}, // response time from {503, 0.0002} of instance 0 plus {200, 0.015} of instance 3
+		{ID: 6, Status: 200, CreatedTime: 0.06, ResponseTime: 0.011, Hops: []string{"i4-f1"}},           // response time from {200, 0.011} of instance 4
+		{ID: 7, Status: 200, CreatedTime: 0.07, ResponseTime: 0.008, Hops: []string{"i3-f0"}},           // response time from {200, 0.008} of instance 3
 	}
 	if len(expectedRequests) != len(simulatedRequests) {
 		log.Fatalf("number of requests - want:%+v got:%+v", len(expectedRequests), len(simulatedRequests))
@@ -66,16 +66,16 @@ func main() {
 		}
 	}
 	instanceData := []struct {
-		id          int
+		id          string
 		createdTime float64
 		upTime      float64
 		efficiency  float64
 	}{
-		{0, 0.00, 0.0361, 0.6398},
-		{1, 0.01, 0.017, 0.6470},
-		{2, 0.0301, 0.0262, 0.3893},
-		{3, 0.0502, 0.030, 0.7700},
-		{4, 0.06, 0.017, 0.6470},
+		{"i0-f0", 0.00, 0.0361, 0.6398},
+		{"i1-f1", 0.01, 0.017, 0.6470},
+		{"i2-f2", 0.0301, 0.0262, 0.3893},
+		{"i3-f0", 0.0502, 0.030, 0.7700},
+		{"i4-f1", 0.06, 0.017, 0.6470},
 	}
 	instancesUsed := res.Instances
 	if len(instanceData) != len(instancesUsed) {
