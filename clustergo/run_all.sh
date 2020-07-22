b2fda5d21becf46f697645c0d4160fa9a23ab957
danielfireman <danielfireman@gmail.com>
2020-07-21 22:22:09 -0300

    Corrigindo run_all e adicionando frequência de 100X

--- clustergo/run_all.sh
+++ clustergo/run_all.sh
@@ -4,96 +4,138 @@ set -x
 
 ###### 1X
 
-# ENABLE_CCT=false BIG_OMEGA=0.003 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE BASELINE 1x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.003 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE BASELINE 1x'
 
-# ENABLE_CCT=false BIG_OMEGA=0.003 HT=847.3845 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE HEDGE 1x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.003 HT=847.3845 HEDGE_CANCELLATION=false NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE HEDGE 1x'
 
-# ENABLE_CCT=true BIG_OMEGA=0.003 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE CTC 1x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.003 HT=847.3845 HEDGE_CANCELLATION=true NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE HEDGE CANC 1x'
+
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=true LITTLE_OMEGA=0.0001 BIG_OMEGA=0.003 HT=-1  NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE CTC 1x'
 
 # ###### 10X
 
-# ENABLE_CCT=false BIG_OMEGA=0.03 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_10x/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_10x/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE BASELINE 10x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_10x/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.03 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_10x/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_10x/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE BASELINE 10x'
 
-# ENABLE_CCT=false BIG_OMEGA=0.03 HT=1046.561 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_10x/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_10x/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE HEDGE 10x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_10x/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.03 HT=1046.561 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_10x/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_10x/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE HEDGE 10x'
 
-# ENABLE_CCT=true BIG_OMEGA=0.03 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_10x/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_10x/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE CTC 10x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc_10x/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.03 HT=847.3845 HEDGE_CANCELLATION=true NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc_10x/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc_10x/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE HEDGE CANC 10x'
+
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_10x/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=true LITTLE_OMEGA=0.0001 BIG_OMEGA=0.03 HT=-1  NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_10x/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_10x/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE CTC 10x'
 
 # ###### 100X
 
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_100x/
+cd ~/old/fireman/repos/gci-simulator/clustergo
 ENABLE_CCT=false BIG_OMEGA=0.3 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
 mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_100x/
 cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/baseline_100x/
 for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
 cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
 cat sim.out | shuf -n 1000000 >> sim_sample.csv
-cd -
 aplay /usr/share/sounds/speech-dispatcher/test.wav
 notify-send 'DONE BASELINE 100x'
 
-# ENABLE_CCT=false BIG_OMEGA=0.3 HT=847.3845 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_100x/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_100x/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE HEDGE 100x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_100x/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.3 HT=847.3845 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_100x/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_100x/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE HEDGE 100x'
 
-# ENABLE_CCT=true BIG_OMEGA=0.3 HT=-1 NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
-# mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_100x/
-# cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_100x/
-# for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
-# cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
-# cat sim.out | shuf -n 1000000 >> sim_sample.csv
-# cd -
-# aplay /usr/share/sounds/speech-dispatcher/test.wav
-# notify-send 'DONE CTC 100x'
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc_100x/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=false BIG_OMEGA=0.3 HT=847.3845 HEDGE_CANCELLATION=true NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc_100x/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/hedge_canc_100x/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE HEDGE CANC 100x'
+
+rm -rf /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_100x/*
+cd ~/old/fireman/repos/gci-simulator/clustergo
+ENABLE_CCT=true LITTLE_OMEGA=0.0001 BIG_OMEGA=0.3 HT=-1  NREPLICAS=2,4,8,16,32,64 ./run_exp.sh
+mv sim_* /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_100x/
+cd /home/danielfireman/tese/resultados/data_cmp_sim_hedge/ctc_100x/
+for i in 2 4 8 16 32 64; do unzip -o sim_${i}_out.zip; done
+cat r*.out | grep -h -w "^[[:digit:]].*\,[503\,|200\,].*" > sim.out
+cat sim.out | shuf -n 1000000 >> sim_sample.csv
+aplay /usr/share/sounds/speech-dispatcher/test.wav
+notify-send 'DONE CTC 100x'
